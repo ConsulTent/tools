b5f4232632df2624c6be8b0116be03591681d522
Alexey Zilber <AlexeyZilber@gmail.com>
2018-08-08 14:45:11 +0800

    Haproxy and openssl related

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tools
 Just some regular tools for some regular stuff
+
+This will be (hopefully) a growing collection of random tools to made SysAdmin/DevOps life easier.
+
+
+## Contents
+------
+
+  * haproxy/generate-haproxy-ssl_fc.sh
+> Generate a forced SSL block from a running site's SSL cert.  Just a SAN cert on a site,
+> run this script, then stick the output into your haproxy.conf for per-domain 301 redirects.
+*Usage*
+```
+./generate-haproxy-ssl_fc.sh <domain>
+Where <domain> is your domain.  You can test with google.com.  Make sure openssl, awk and
+standard Linux friends are installed.
+```
+
+
+
---
+++ haproxy/generate-haproxy-ssl_fc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# (C) 2018 Alexey Zilber @ ConsulTent, LTD - azilber@consultent.ltd
+
+args=("$@")
+
+if [ "${args[0]}" == "" ]; then
+echo "Missing domain name to extract cert data from."
+echo "Example: ./generate-haproxy-ssl_fc.sh google.com"
+exit
+fi
+
+echo '#Copy/Paste everything below this line into a bind block in haproxy...'
+echo '#SSL force related'
+echo 'http-request add-header X-Proto https if { ssl_fc }'
+echo 'http-request set-header X-Forwarded-Port %[dst_port]'
+echo 'http-request add-header X-Forwarded-Proto https if { ssl_fc }'
+echo " "
+echo '#Domain block: '
+
+for i in $(openssl s_client -showcerts -connect ${args[0]}:443 </dev/null 2>/dev/null | openssl x509 -noout -text | awk '/X509v3 Subject Alternative Name/ {getline;gsub(/ /,"",$0);gsub(/DNS:/,"",$0);gsub(/IPAddress:/,"",$0);gsub(/\*\./,"",$0);gsub(/\,/,"\n",$0); print}' | sort | uniq ); { 
+
+echo "redirect scheme https code 301 if { hdr_end(Host) -i $i } !{ ssl_fc }";
+}
+echo '#End Domain block'
+
